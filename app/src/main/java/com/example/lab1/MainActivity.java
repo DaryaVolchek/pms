bfb0ba9bc2f4f686060826134b7229cd816a7e07
Darya Volchek <darya.solig.01@gmail.com>
2022-09-30 20:06:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import text.TextFunction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " counter =" + count);
+        }
+        
+        displayText();
+    }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
+    }
+}
